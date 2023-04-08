05071a09347f5200692f95bbaab25937c851f2e8
swathivetri <126381449+swathivetri@users.noreply.github.com>
2023-04-08 18:29:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs = require('fs');
+const path = require("path");
+const server=http.createServer((req, res) => {
+     const url = req.url;
+     const method = req.method;
+     const body = [];
+
+     if(url === "/") {
+
+        
+        fs.readFile("message.txt", { encoding: "utf-8" },(err, data) => {
+            if(err){
+                console.log(err);
+    }
+      console.log('data from file' + data);
+      res.write("<html>");
+      res.write("<head><title>Enter Message</title><head>");
+      res.write('<body>${data},</body>');
+      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+      res.write("</html>");
+      return res.end();
+        });
+    }
+    else if(url === '/message' && method === 'POST') {
+        req.on("data",(chunk) => {
+         body.push(chunk);
+        });
+
+        return req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split('=')[1];
+        fs.writeFile("message.text", message, (err) => {
+            if(err) {
+                console.log(err)
+            }
+            console.log("inside fs.writeFile");
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end(); 
+      });    
+  });        
+}
+else {
+     res.setHeader('Content-Type', 'text/html');
+     res.write('<html>');
+     res.write('<head><title>My First Page</title><head>');
+     res.write('<body><h1>SwathiRam</h1></body>');
+     res.write('</html>');
+     res.end();
+   }
+});
+
+   
+     server.listen(3000);
+   
